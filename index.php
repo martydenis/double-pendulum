9dac09e1ad025e0c833cd0bb8f40b9c194d5b8ab
Martin Denis <martin.2nis@gmail.com>
2023-11-30 13:15:15 +0100

    Added controls panel

--- index.php
+++ index.php
@@ -2,17 +2,11 @@
 <!DOCTYPE html>
 <html lang="<?php echo $language_iso; ?>">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="icon" type="image/svg" href="<?php echo $favicon_path; ?>favicon.svg" />
-    <link rel="stylesheet" type="text/css" href="<?php echo $css_path; ?>game.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="./index.css" media="screen" />
+    <?php include_once '../../_games_head.php'; ?>
     <title>Double pendulum</title>
   </head>
 
   <body id="double-pendulum">
-    <a href="<?php echo $sandbox_path; ?>" id="back">Back</a>
-
     <canvas id="canvas" width="400" height="400"></canvas>
     <canvas id="canvas-line" width="400" height="400"></canvas>
 
